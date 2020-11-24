3d32b0f9e6736f515abfa2c2d04fa6bf8696d781
Patrik Švikruha <patrik.svikruha@gmail.com>
2020-11-24 13:40:14 +0100

    Move web worker to Worker folder

--- Worker/worker.js
+++ Worker/worker.js
@@ -4,10 +4,3 @@ onmessage = function (e) {
     console.log('Posting message back to main script');
     postMessage(workerResult);
 }
-
-onmessage = function (e) {
-  console.log('Message received from main script');
-  var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
-}
