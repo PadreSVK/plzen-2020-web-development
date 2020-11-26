bbb79d0ae03d81ebe563f1fdb69d1e0d5c27ae04
Patrik Švikruha <patrik.svikruha@gmail.com>
2020-11-26 09:21:01 +0100

    merge

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,6 +1,6 @@
 interface User {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 }
 
 let myString: string = "asdasd";
@@ -12,20 +12,25 @@ if (myString == "") {
 
 
 const user = {
-  name: "Hayes",
-  id: 0,
+    name: "Hayes",
+    id: 44
+};
+
+const user2: User = {
+    name: "Hayes",
+    id: 44
 };
 
 type UserFromJson = typeof user
 
-const userFromInterface: User = {
-  name: "Hayes",
-  id: 0,
+const userFromInterface: UserFromJson = {
+    name: "Hayes",
+    id: 44
 };
 
 
-function test(myArgument: User): string {
-  return "Hi from function"
+function test(myArgument: { name: string, id: number }): string {
+    return "Hi from function"
 }
 
 test(user)
@@ -34,23 +39,76 @@ test({ id: 444, name: "asdas" })
 
 
 class UserAccount {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 
-  constructor(name: string, id: number) {
-    this.name = name;
-    this.id = id;
-  }
+    constructor(name: string, id: number = 55) {
+        this.name = name;
+        this.id = id;
+    }
 }
 
 
 
 // const userAccount = new UserAccount()
-const userAccount = new UserAccount("aaa", 999)
+const userAccount = new UserAccount("aaa", 99)
 
 
 // Composing Types
+
+type AvatarType = "admin" | "manager" | "tester" | "user"
+
+const avatars = {
+    admin: {
+        name: "Fero",
+        age: 5
+    },
+    manager: {
+        name: "Jozo",
+        age: 5
+    },
+    tester: {
+        name: "Kevin",
+        age: 5
+    },
+    user: {
+        name: "Patrik",
+        age: 5
+    },
+}
+
+
+type Avatars = typeof avatars
+type Avatar = typeof avatars.admin
+
+function chooseAvatar(avatars: Avatars, choosedAvatar: AvatarType): Avatar {
+    return avatars[choosedAvatar]
+}
+
+const choosedAvatar = chooseAvatar(avatars, "user")
+
+
+type ComplexComposedType = { name: string } | { bigName: string, age?: number }
+
+
+const myVar1: ComplexComposedType = {
+    name: "sad"
+}
+
+const myVar2: ComplexComposedType = {
+    name: "sad",
+    age: 444
+}
+
+const myVar3: ComplexComposedType = {
+    bigName: "Fero",
+    age: 444
+}
+
 type WindowStates = "open" | "closed" | "minimized";
+
+
+
 type LockStates = "locked" | "unlocked";
 type OddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
 
@@ -58,10 +116,13 @@ function compositionTest(myArgument: WindowStates) {
 
 }
 
+const myArg5 : OddNumbersUnderTen = 5; 
+
+
 compositionTest("minimized")
 
 async function loadModule() {
-  return await import("./module");
+    return await import("./module");
 }
 
 
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@
 * [VSCode](https://code.visualstudio.com/)
 * [nodejs](https://nodejs.org/en/download/)
     * [http-server](https://www.npmjs.com/package/http-server)
+
+## Materials
